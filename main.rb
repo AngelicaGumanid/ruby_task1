93d750125e9078ba7e3727499dd2c7c6e020bb6d
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-13 10:47:59 +0800

    print the entire list of array of people.

--- main.rb
+++ main.rb
@@ -1,1 +1,26 @@
-puts 'Hello World'
+persons = {
+  person_1: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_2: { field: "Architect", national_id: "12345", name: "Aia", age: "25" },
+  person_3: { field: "Software Developer", national_id: "12345", name: "Aia", age: "25" },
+  person_4: { field: "Field", national_id: "12345", name: "Aia", age: "25" },
+  person_5: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_6: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_7: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_8: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_9: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_10: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_11: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_12: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_13: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_14: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_15: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_16: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_17: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_18: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_19: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" },
+  person_20: { field: "Doctor", national_id: "12345", name: "Aia", age: "25" }
+}
+
+persons.each do |key, value|
+  puts "Information: #{key}, #{value}"
+end
